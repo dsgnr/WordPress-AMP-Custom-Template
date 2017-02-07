14208ff9936b4532d5db6e5098bf5e3f004b152c
Daniel Hand <email@danielhand.io>
2017-02-07 08:16:13 +0000

    Initial commit

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="amp-wp-footer">
+	<div>
+		<p>
+ <p>© 2017 <a href="https://www.speakwp.com" >SpeakWP</a>. All rights reserved.</p>
+		</p>
+		<a href="#top" class="back-to-top"><?php _e( 'Back to top', 'amp' ); ?></a>
+	</div>
+</footer>
---
+++ header-bar.php
@@ -0,0 +1,13 @@
+<header id="#top" class="amp-wp-header">
+    <div>
+        <div class="branding">
+        	<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+          		<span class="brand-logo text dark">
+ 				<amp-img src="https://www.speakwp.com/speak-wp-logo.svg" width="150px" height="50px"></amp-img>
+          		</span>
+          	</a>
+	</div>
+    </div>
+</header>
+
+
